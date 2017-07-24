c710307dab985dfa622c5ae87c84dbe101e35152
Andreas Göbel <andreas.goebel@typeofnan.com>
2017-07-24 03:13:59 +0200

    - removed old code block - turned width from 100% to auto for intro video

--- app.js
+++ app.js
@@ -7,25 +7,3 @@ const console	= new LogTools({ id: 'app' });
 
 console.log('starting topSection...: ');
 topSection.start();
-
-/*
-document.getElementById( 'foo' ).addEventListener( 'click', function( event ) {
-	alert('requesting... via import()');
-
-	import( /* webpackChunkName: "TopSection" / './modules/topsection/js/main.js' ).then( TopSection => {
-		console.log( 'main.js transfered and evaluated, value: ', TopSection );
-		TopSection.start();
-	}).catch( err => {
-		console.log('ERR: ', err);
-	});
-	console.log('!!IF THIS WRITES BEFORE LOADED HINT, IT IS ASYNC!');
-}, false);
-
-setTimeout(function() {
-	import( /* webpackChunkName: "Module2" / './modules/mod2/entry.js' ).then( MOD2 => {
-		console.log('REALLY NEWSTYLE mod2.js was transfered and now gets evaluated immediately: ', MOD2);
-	}).catch( err => {
-		console.log('ERR: ', err);
-	});
-}, 2000);
-*/
