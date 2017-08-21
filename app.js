bd34d62de364c0baa8e4acdf7bb1f146ac8ad9e8
Andreas Göbel <andreas.goebel@judgemy.org>
2017-08-21 18:21:41 +0200

    - added background image to git - updated package.json - introduced Composition method

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import * as topSection from './modules/topsection/js/main.js';
-import { mix } from 'barfoos2.0/toolkit.js';
+import { Composition } from 'barfoos2.0/toolkit.js';
 import { LogTools } from 'barfoos2.0/domkit.js';
 import { Mediator } from 'barfoos2.0/mediator.js';
 
-class DVGWebsite extends mix().with( Mediator, LogTools ) {
+class DVGWebsite extends Composition( Mediator, LogTools ) {
 	constructor() {
 		super( ...arguments );
 		this.init();
@@ -16,7 +16,7 @@ class DVGWebsite extends mix().with( Mediator, LogTools ) {
 		this.fire( 'configApp.core', {
 			name:				'Der Vegane Germane - Website',
 			title:				'Der Vegane Germane',
-			version:			'0.0.2',
+			version:			'0.0.4',
 			status:				'alpha',
 			background:			{
 				image:		'/images/background.jpg',
